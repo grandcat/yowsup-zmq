c802cb0c73c8092e0431e24292efb629ecbf7234
Stefan Smarzly <smarzlys@gmail.com>
2016-02-25 22:31:36 +0100

    Two examples for sending a message and creating a group

---
+++ examples/test_message_send.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+import asyncio
+import zmq
+import zmq.asyncio
+
+ctx = zmq.asyncio.Context()
+loop = zmq.asyncio.ZMQEventLoop()
+asyncio.set_event_loop(loop)
+
+@asyncio.coroutine
+def recv_and_process():
+    sock = ctx.socket(zmq.REQ)
+    sock.connect("tcp://127.0.0.1:%s" % 5555)
+    print("Waiting...")
+
+    # Send message to number
+    yield from sock.send_json({"cmd": "message_send",
+                               "number": "49176123456",
+                               "msg": "How are you? :)"})
+    # Wait for result and print it
+    result = yield from sock.recv_multipart()
+    print(result[0].decode())
+
+loop.run_until_complete(recv_and_process())
